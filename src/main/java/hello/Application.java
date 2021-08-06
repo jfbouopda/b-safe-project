6374bbfd66af2ab5107500745ad374b624a48330
jfbouopda <75217658+jfbouopda@users.noreply.github.com>
2021-08-06 15:44:53 +0200

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World";
+		return "Hello Docker World by Joseph Francois Bouopda - Simplilearn 2021";
 	}
 
 	public static void main(String[] args) {
